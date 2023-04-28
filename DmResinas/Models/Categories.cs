d088ecbf9a9f1df30446ba1700fbc7fe9076d0d5
Bard0u <pedroarossettoo@gmail.com>
2023-04-28 12:23:44 -0300

    Iniciado o Banco de dados

---
+++ DmResinas/Models/Categories.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DmResinas.Models
+{
+    [Table("Categories")]
+    public class Categories
+    {
+        [Key]
+        public byte CategoriesId { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O Nome da categoria é obrigatorio")]
+        [StringLength(30, ErrorMessage = "O nome da categoria deve possuir no maximo 30 caracteres")]
+        public string CategorieName { get; set; }
+
+    }
+}
