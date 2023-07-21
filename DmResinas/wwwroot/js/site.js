5558516b68df1da06550caa7e829338402dfda41
Kênia De Marchi <keniademarchi@gmail.com>
2023-07-21 00:37:14 -0300

    Carrossel e outros detalhes

--- DmResinas/wwwroot/js/site.js
+++ DmResinas/wwwroot/js/site.js
@@ -1,1 +1,94 @@
-﻿
+﻿const $ = selector => {
+    return document.querySelector(selector);
+  };
+  
+  function next() {
+    if ($(".prev3")) {
+      $(".prev3").classList.add("next");
+      $(".prev3").remove(); 
+    }
+    
+    if ($(".prev1")) {
+      $(".prev1").classList.add("prev2");
+      $(".prev1").classList.remove("prev1");
+    }
+    
+    if ($(".prev2")) {
+      $(".prev2").classList.add("prev3");
+      $(".prev2").classList.remove("prev2");
+    }
+  
+    /* Step */
+  
+    if ($(".prev")) {
+      $(".prev").classList.add("prev1");
+      $(".prev").classList.remove("prev");
+    }
+  
+    $(".act").classList.add("prev");
+    $(".act").classList.remove("act");
+  
+    $(".next").classList.add("act");
+    $(".next").classList.remove("next");
+  
+    /* New Next */
+  
+    $(".new-next").classList.remove("new-next");
+  
+    const addedEl = document.createElement('li');
+  
+    $(".list").appendChild(addedEl);
+    addedEl.classList.add("next","new-next");
+  }
+  
+  function prev() {
+    $(".new-next").remove();
+      
+    /* Step */
+  
+    $(".next").classList.add("new-next");
+  
+    $(".act").classList.add("next");
+    $(".act").classList.remove("act");
+  
+    $(".prev").classList.add("act");
+    $(".prev").classList.remove("prev");
+  
+    /* New Prev */
+  
+    $(".hide").classList.add("prev");
+    $(".hide").classList.remove("hide");
+  
+    const addedEl = document.createElement('li');
+  
+    $(".list").insertBefore(addedEl, $(".list").firstChild);
+    addedEl.classList.add("hide");
+  }
+  
+  slide = element => {
+    /* Next slide */
+    
+    if (element.classList.contains('next')) {
+      next();
+      
+    /* Previous slide */
+      
+    } else if (element.classList.contains('prev')) {
+      prev();
+    }
+  }
+  
+  const slider = $(".list"),
+        swipe = new Hammer($(".swipe"));
+  
+  slider.onclick = event => {
+    slide(event.target);
+  }
+  
+  swipe.on("swipeleft", (ev) => {
+    next();
+  });
+  
+  swipe.on("swiperight", (ev) => {
+    prev();
+  });
