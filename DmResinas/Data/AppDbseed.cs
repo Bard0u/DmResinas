051e7e0fa72c70c7870127fed8e0847708100d60
Kênia De Marchi <keniademarchi@gmail.com>
2023-11-21 21:04:01 -0300

    Finalizando dicas e cadastrando cores/categorias

--- DmResinas/Data/AppDbseed.cs
+++ DmResinas/Data/AppDbseed.cs
@@ -12,32 +12,197 @@ public class AppDbSeed
             new Cor() {
                 Id = 1,
                 Nome = "Preto",
-                CodigoHexa = "#222"
+                CodigoHexa = "#000000"
             },
             new Cor() {
                 Id = 2,
-                Nome = "Azul",
-                CodigoHexa = "#4272d7"
+                Nome = "Amarelo",
+                CodigoHexa = "#efca3e"
             },
             new Cor() {
                 Id = 3,
                 Nome = "Cinza",
-                CodigoHexa = "#b3b3b3"
+                CodigoHexa = "#9399a7"
             },
             new Cor() {
                 Id = 4,
-                Nome = "Verde",
-                CodigoHexa = "#00ad5f"
+                Nome = "Verde Água",
+                CodigoHexa = "#85dedc"
             },
             new Cor() {
                 Id = 5,
                 Nome = "Vermelho",
-                CodigoHexa = "#fa4251"
+                CodigoHexa = "#bd1600"
             },
             new Cor() {
                 Id = 6,
                 Nome = "Branco",
-                CodigoHexa = "#aaa"
+                CodigoHexa = "#ffffff"
+            },
+            new Cor() {
+                Id = 7,
+                Nome = "Azul Turquesa",
+                CodigoHexa = "#1b4a82"
+            },
+            new Cor() {
+                Id = 8,
+                Nome = "Azul Candy",
+                CodigoHexa = "#7ab5f9"
+            },
+            new Cor() {
+                Id = 9,
+                Nome = "Bege",
+                CodigoHexa = "#7ab5f9"
+            },
+            new Cor() {
+                Id = 10,
+                Nome = "Amarelo Vintage",
+                CodigoHexa = "#efe48a"
+            },
+            new Cor() {
+                Id = 11,
+                Nome = "Lilás",
+                CodigoHexa = "#978ec5"
+            },
+            new Cor() {
+                Id = 12,
+                Nome = "Rosa Flamingo",
+                CodigoHexa = "#df98b9"
+            },
+            new Cor() {
+                Id = 13,
+                Nome = "Branco Perolado",
+                CodigoHexa = "#ffffff"
+            },
+            new Cor() {
+                Id = 14,
+                Nome = "Cinza Perolado",
+                CodigoHexa = "#524f40"
+            },
+            new Cor() {
+                Id = 15,
+                Nome = "Verde Escuro Perolado",
+                CodigoHexa = "#20362d"
+            },
+            new Cor() {
+                Id = 16,
+                Nome = "Azul Escuro Perolado",
+                CodigoHexa = "#001386"
+            },
+            new Cor() {
+                Id = 17,
+                Nome = "Azul Perolado",
+                CodigoHexa = "#034edb"
+            },
+            new Cor() {
+                Id = 18,
+                Nome = "Azul Claro Perolado",
+                CodigoHexa = "#2890e5"
+            },
+            new Cor() {
+                Id = 19,
+                Nome = "Marrom Escuro Perolado",
+                CodigoHexa = "#835426"
+            },
+            new Cor() {
+                Id = 20,
+                Nome = "Marrom Perolado",
+                CodigoHexa = "#5e4624"
+            },
+            new Cor() {
+                Id = 21,
+                Nome = "Bronze Perolado",
+                CodigoHexa = "#835426"
+            },
+            new Cor() {
+                Id = 22,
+                Nome = "Marrom Claro Perolado",
+                CodigoHexa = "#ac7032"
+            },
+            new Cor() {
+                Id = 23,
+                Nome = "Laranja Perolado",
+                CodigoHexa = "#f03b04"
+            },
+            new Cor() {
+                Id = 24,
+                Nome = "Amarelo Perolado",
+                CodigoHexa = "#e8ce33"
+            },
+            new Cor() {
+                Id = 25,
+                Nome = "Dourado Claro Perolado",
+                CodigoHexa = "#e0a621"
+            },
+            new Cor() {
+                Id = 26,
+                Nome = "Dourado Perolado",
+                CodigoHexa = "#cc9900"
+            },
+            new Cor() {
+                Id = 27,
+                Nome = "Roxo Perolado",
+                CodigoHexa = "#581598"
+            },
+            new Cor() {
+                Id = 28,
+                Nome = "Rosa Claro Perolado",
+                CodigoHexa = "#ffb2d1"
+            },
+            new Cor() {
+                Id = 29,
+                Nome = "Rosa Chiclete Perolado",
+                CodigoHexa = "#e17282"
+            },
+            new Cor() {
+                Id = 30,
+                Nome = "Pink Perolado",
+                CodigoHexa = "#cb3038"
+            },
+            new Cor() {
+                Id = 31,
+                Nome = "Rosa Perolado",
+                CodigoHexa = "#cc2862"
+            },
+            new Cor() {
+                Id = 32,
+                Nome = "Rosa Choque Perolado",
+                CodigoHexa = "#f3134f"
+            },
+            new Cor() {
+                Id = 33,
+                Nome = "Cobre Perolado",
+                CodigoHexa = "#a31208"
+            },
+            new Cor() {
+                Id = 34,
+                Nome = "Verde Fluorescente Perolado",
+                CodigoHexa = "#4fdb22"
+            },
+            new Cor() {
+                Id = 35,
+                Nome = "Verde Esmeralda Perolado",
+                CodigoHexa = "#06785f"
+            },
+            new Cor() {
+                Id = 36,
+                Nome = "Verde Água Perolado",
+                CodigoHexa = "#38ab72"
+            },
+            new Cor() {
+                Id = 37,
+                Nome = "Verde Bandeira Perolado",
+                CodigoHexa = "#1d5236"
+            },
+            new Cor() {
+                Id = 38,
+                Nome = "Verde Claro Perolado",
+                CodigoHexa = "#9dbd69"
+            },
+            new Cor() {
+                Id = 39,
+                Nome = "Magenta Perolado",
+                CodigoHexa = "#d4124e"
             }
         };
         builder.Entity<Cor>().HasData(cores);
@@ -46,28 +211,28 @@ public class AppDbSeed
         List<Categoria> categorias = new() {
             new Categoria() {
                 Id = 1,
-                Nome = "Feminina",
+                Nome = "Folha de Ouro",
                 Foto = @"images/categorias/1.jpg",
                 Filtrar = true,
                 Banner = true
             },
             new Categoria() {
                 Id = 2,
-                Nome = "Masculina",
+                Nome = "Flores",
                 Foto = @"images/categorias/2.jpg",
                 Filtrar = true,
                 Banner = true
             },
             new Categoria() {
                 Id = 3,
-                Nome = "Acessórios",
+                Nome = "Decoração",
                 Foto = @"images/categorias/3.jpg",
                 Filtrar = false,
                 Banner = true
             },
             new Categoria() {
                 Id = 4,
-                Nome = "Bolsas",
+                Nome = "Chaveiros",
                 Foto = "",
                 Filtrar = true,
                 Banner = false,
@@ -75,7 +240,7 @@ public class AppDbSeed
             },
             new Categoria() {
                 Id = 5,
-                Nome = "Calçados",
+                Nome = "Marca Páginas",
                 Foto = "",
                 Filtrar = true,
                 Banner = false,
@@ -83,7 +248,15 @@ public class AppDbSeed
             },
             new Categoria() {
                 Id = 6,
-                Nome = "Relógios",
+                Nome = "Com Nome",
+                Foto = "",
+                Filtrar = true,
+                Banner = false,
+                CategoriaPaiId = 3
+            },
+            new Categoria() {
+                Id = 6,
+                Nome = "Placa",
                 Foto = "",
                 Filtrar = true,
                 Banner = false,
--- DmResinas/wwwroot/js/sobre.js
+++ DmResinas/wwwroot/js/sobre.js
@@ -1,18 +1,15 @@
 /* PAGINA SOBRE */
 
 const boxes = document.querySelectorAll('.datesSobre, .infoSobre');
-console.log("oi");
 window.addEventListener('scroll', checkBoxes);
-console.log('ae');
 checkBoxes();
 
 function checkBoxes() {
   const triggerBottom = window.innerHeight / 5 * 4;
-  console.log('ae');
   boxes.forEach(box => {
     const boxTop = box.getBoundingClientRect().top;
 
-    if (boxTop < triggerBottom) {
+    if (boxTop < triggerBottom - 50) {
       box.classList.add('mostrar');
     } else {
       box.classList.remove('mostrar')
